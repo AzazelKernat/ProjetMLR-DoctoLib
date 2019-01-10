df11ac985282fc4d1f72e15f7fcb1296caa8d679
Lea <pelissette@hotmail.fr>
2019-01-10 14:30:47 +0100

    Application implementé

--- ProjetMLR-V1/ProjetMLR-V1/src/main/java/application/Application.java
+++ ProjetMLR-V1/ProjetMLR-V1/src/main/java/application/Application.java
@@ -1,5 +1,30 @@
 package application;
 
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+//import dao.jpa.DaoPatientJpa;
+
+
+
 public class Application {
+	
+	private static Application instance = null;
+
+	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("projetmlr");
+//	private final IDaoPat daoMatiere = new DaoPatientJpa();
+	
+	public static Application getInstance() {
+		if (instance == null) {
+			instance = new Application();
+		}
+
+		return instance;
+	}
+
+	public EntityManagerFactory getEmf() {
+		return emf;
+	}
 
 }
